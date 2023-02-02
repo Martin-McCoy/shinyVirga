b728c13e7b21df51c1fcb9d7169e22554b3f8e74
Stephen Holsenbeck <stephen@virgalabs.io>
2023-02-02 16:31:52 -0500

    `icon_sb`: now supports _img_ tags with class to be associated with `*_path`s to the images

--- R/utils_icons.R
+++ R/utils_icons.R
@@ -2,15 +2,21 @@
 #'
 #' @param name \code{chr} FontAwesome name or image path
 #' @inherit shiny::icon params return
+#' @param inactive_path \code{chr} path to the icon image file in it's inactive (unselected) state, starting with `www`.
+#' @param active_path \code{chr} path to the icon image file in it's active (selected) state, starting with `www`.
 #' @family ui
 #' @export
 
-icon_sb <- function(name, class = NULL, lib = "font-awesome", ...) {
-  if (!missing(name) && UU::`%|try|%`(file.exists(name), FALSE)) {
+icon_sb <- function(name, class = NULL, lib = "font-awesome", inactive_path = NULL, active_path = NULL, inactive_props = NULL, active_props = inactive_props, ...) {
+  msg_nm <- missing(name)
+  if (msg_nm && !is.null(class) && is.null(inactive_path)) {
+    UU::gwarn("{.code class} supplied but no {.code inactive_path} supplied. If these are img icons, please supply {.code inactive_path} with the path to the image.")
+  }
+  out <- if (!msg_nm && UU::`%|try|%`(file.exists(name), FALSE)) {
     shiny::img(src = path_strip_to(name, "www"),
                class = class,
                ...)
-  } else if (missing(name)) {
+  } else if (msg_nm) {
     shiny::img(class = class, ...)
   } else
     shiny::icon(
@@ -20,6 +26,38 @@ icon_sb <- function(name, class = NULL, lib = "font-awesome", ...) {
       ...,
       verify_fa = FALSE
     )
+  # Make CSS when icons are images
+  css = c()
+  if (!is.null(inactive_path)) {
+    # These wierd paths (../..) for content:url are necessary to make the images work on shinyapps.io
+    css <- UU::glue_js(
+      "
+   .*{class}* {
+     content:url('*{fs::path('../..', inactive_path)}*');
+    *{css_props(declarations = inactive_props)}*
+   }"
+   )
+  }
+
+  if (!is.null(active_path)) {
+    css <- c(css, UU::glue_js(
+      "
+   .active .*{class}* {
+     content:url('*{fs::path('../..', active_path)}*');
+     *{css_props(declarations = active_props)}*
+     }"
+    ))
+  }
+  if (UU::is_legit(css)) {
+    out <- htmltools::tagAppendChild(
+      out,
+      htmltools::tags$style(
+        type = "text/css",
+        glue::glue_collapse(css, sep = "\n")
+      )
+    )
+  }
+  return(out)
 }
 
 #' FontAwesome Arrow Icons
